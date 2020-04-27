1cf00980bdb94269fc16b3fc5656c8fd7d412336
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-27 15:00:27 +0800

    more

--- programming-elm/getting-started/picshare/src/Picshare.elm
+++ programming-elm/getting-started/picshare/src/Picshare.elm
@@ -3,16 +3,26 @@ module Picshare exposing (main)
 import Html exposing (Html, div, h1, h2, text, img)
 import Html.Attributes exposing (class, src)
 
+baseUrl : String
+baseUrl = "https://programming-elm.com/"
+
 main : Html msg
 main = 
   div [] 
     [ div [class "header"][
       h1 [][text "Picshare"]]
     , div [class "content-flow"]
-      [ div [class "detailed-photo"]
-      [ img [src "https://programming-elm.com/1.jpg"][]
-      , div [class "photo-info"] 
-        [h2 [class "caption"][text "Surfing"]]
+      [ viewDetailedPhoto (baseUrl ++ "1.jpg") "Surfing"
+      , viewDetailedPhoto (baseUrl ++ "2.jpg") "The Fox"
+      , viewDetailedPhoto (baseUrl ++ "3.jpg") "Evening"
       ]
     ]
-  ]
+
+viewDetailedPhoto : String -> String -> Html msg
+viewDetailedPhoto url caption =
+  div [class "detailed-photo"]
+    [ img [src url][]
+    , div [class "photo-info"]
+      [ h2 [class "caption"][text caption]
+      ]
+    ]
--- programming-elm/getting-started/picshare/src/picshare.js
+++ programming-elm/getting-started/picshare/src/picshare.js
@@ -4404,6 +4404,7 @@ var $elm$virtual_dom$VirtualDom$toHandlerInt = function (handler) {
 			return 3;
 	}
 };
+var $author$project$Picshare$baseUrl = 'https://programming-elm.com/';
 var $elm$json$Json$Encode$string = _Json_wrap;
 var $elm$html$Html$Attributes$stringProperty = F2(
 	function (key, string) {
@@ -4415,6 +4416,8 @@ var $elm$html$Html$Attributes$stringProperty = F2(
 var $elm$html$Html$Attributes$class = $elm$html$Html$Attributes$stringProperty('className');
 var $elm$html$Html$div = _VirtualDom_node('div');
 var $elm$html$Html$h1 = _VirtualDom_node('h1');
+var $elm$virtual_dom$VirtualDom$text = _VirtualDom_text;
+var $elm$html$Html$text = $elm$virtual_dom$VirtualDom$text;
 var $elm$html$Html$h2 = _VirtualDom_node('h2');
 var $elm$html$Html$img = _VirtualDom_node('img');
 var $elm$html$Html$Attributes$src = function (url) {
@@ -4423,8 +4426,44 @@ var $elm$html$Html$Attributes$src = function (url) {
 		'src',
 		_VirtualDom_noJavaScriptOrHtmlUri(url));
 };
-var $elm$virtual_dom$VirtualDom$text = _VirtualDom_text;
-var $elm$html$Html$text = $elm$virtual_dom$VirtualDom$text;
+var $author$project$Picshare$viewDetailedPhoto = F2(
+	function (url, caption) {
+		return A2(
+			$elm$html$Html$div,
+			_List_fromArray(
+				[
+					$elm$html$Html$Attributes$class('detailed-photo')
+				]),
+			_List_fromArray(
+				[
+					A2(
+					$elm$html$Html$img,
+					_List_fromArray(
+						[
+							$elm$html$Html$Attributes$src(url)
+						]),
+					_List_Nil),
+					A2(
+					$elm$html$Html$div,
+					_List_fromArray(
+						[
+							$elm$html$Html$Attributes$class('photo-info')
+						]),
+					_List_fromArray(
+						[
+							A2(
+							$elm$html$Html$h2,
+							_List_fromArray(
+								[
+									$elm$html$Html$Attributes$class('caption')
+								]),
+							_List_fromArray(
+								[
+									$elm$html$Html$text(caption)
+								]))
+						]))
+				]));
+	});
 var $author$project$Picshare$main = A2(
 	$elm$html$Html$div,
 	_List_Nil,
@@ -4454,41 +4493,9 @@ var $author$project$Picshare$main = A2(
 				]),
 			_List_fromArray(
 				[
-					A2(
-					$elm$html$Html$div,
-					_List_fromArray(
-						[
-							$elm$html$Html$Attributes$class('detailed-photo')
-						]),
-					_List_fromArray(
-						[
-							A2(
-							$elm$html$Html$img,
-							_List_fromArray(
-								[
-									$elm$html$Html$Attributes$src('https://programming-elm.com/1.jpg')
-								]),
-							_List_Nil),
-							A2(
-							$elm$html$Html$div,
-							_List_fromArray(
-								[
-									$elm$html$Html$Attributes$class('photo-info')
-								]),
-							_List_fromArray(
-								[
-									A2(
-									$elm$html$Html$h2,
-									_List_fromArray(
-										[
-											$elm$html$Html$Attributes$class('caption')
-										]),
-									_List_fromArray(
-										[
-											$elm$html$Html$text('Surfing')
-										]))
-								]))
-						]))
+					A2($author$project$Picshare$viewDetailedPhoto, $author$project$Picshare$baseUrl + '1.jpg', 'Surfing'),
+					A2($author$project$Picshare$viewDetailedPhoto, $author$project$Picshare$baseUrl + '2.jpg', 'The Fox'),
+					A2($author$project$Picshare$viewDetailedPhoto, $author$project$Picshare$baseUrl + '3.jpg', 'Evening')
 				]))
 		]));
 _Platform_export({'Picshare':{'init':_VirtualDom_init($author$project$Picshare$main)(0)(0)}});}(this));
