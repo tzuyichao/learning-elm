0659c077fe1476d81504e9dd7616cedf33d959f4
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-27 13:18:57 +0800

    lab

---
+++ programming-elm/getting-started/picshare/src/Picshare.elm
@@ -0,0 +1,9 @@
+module Picshare exposing (main)
+
+import Html exposing (Html, div, h1, text)
+import Html.Attributes exposing (class)
+
+main : Html msg
+main = 
+  div [class "header"][
+    h1 [][text "Picshare"]]
