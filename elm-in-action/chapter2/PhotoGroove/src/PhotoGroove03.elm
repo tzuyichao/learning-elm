f8648f45cc7f81bd4ad9cab3aa2b274d6b2ddb99
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-28 17:10:29 +0800

    partial

--- elm-in-action/chapter2/PhotoGroove/src/PhotoGroove03.elm
+++ elm-in-action/chapter2/PhotoGroove/src/PhotoGroove03.elm
@@ -21,7 +21,7 @@ view : {photos: List {url: String}, selectedUrl: String} -> Html msg
 view model = 
   div [class "content"]
       [ h1 [][text "Photo Groove"]
-      , div [id "thumbnails"] (List.map (viewThumbnail model.selectedUrl) model.photos)
+      , div [id "thumbnails"] (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
       , img [class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl)][]
       ]
 
